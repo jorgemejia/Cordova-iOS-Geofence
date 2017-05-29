c64f600dc7a22aa900df301230faf32ff44e3773
Jorg <jorge_mejia@outlook.com>
2017-05-29 15:23:17 -0500

    Update iOSGeofence.js

--- www/iOSGeofence.js
+++ www/iOSGeofence.js
@@ -1,9 +1,14 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "iOSGeofence", "coolMethod", [arg0]);
+
+var Geofence = {
+    echo :  function(arg0, success, error){
+        exec(success, error, "iOSGeofence", "echo", [arg0]);
+    },
+
+    requestPermission : function(arg0, success, error){
+        exec(success, error, "iOSGeofence", "requestPermission", [arg0]);
+    }
 };
 
-exports.echo = function(arg0, success, error){
-	exec(success, error, "iOSGeofence", "echo", [arg0]);
-}
+module.exports = Geofence;
